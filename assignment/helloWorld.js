d8f0ce43c524182e337e69dc918e6bf20a9efc46
Owen Carruth <ocarr199@gmail.com>
2021-04-03 15:36:48 -0500

    Completed the first assignment

---
+++ assignment/helloWorld.js
@@ -0,0 +1,26 @@
+let name = prompt("Whats your name?")
+alert("Hello " + name)
+
+
+function pictureChange()
+ {
+  document.getElementById("theImage").src="https://i.imgur.com/hgOhKc8.jpg?1";
+ }
+
+
+ function pictureChange1()
+  {
+   document.getElementById("theImage").src="https://i.imgur.com/pBEWQQy.png?1";
+  }
+
+
+  function pictureChange2()
+   {
+    document.getElementById("theImage").src="https://i.imgur.com/6GsmcvI.jpg";
+   }
+
+
+   function pictureChange3()
+    {
+     document.getElementById("theImage").src="https://i.imgur.com/9sC9jEd.jpg";
+    }
